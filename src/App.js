9949164f93f16e8dc3ae5f1a48989ec3d872fe33
Bojan Golubovic <bojan@neopix.rs>
2018-01-14 18:09:55 +0100

    Add and disaply blocks from Redux store

--- src/App.js
+++ src/App.js
@@ -11,19 +11,25 @@ let mapStateToProps = (store) => {
 
 class App extends Component {
   componentDidMount(){
-    window.addEventListener('scroll', this.loadMore, false);
+    // window.addEventListener('scroll', this.loadMore, false);
   }
 
   loadMore(){
     this.props.dispatch(loadMore(20));
   }
+
   render() {
-    console.log(this);
+    const { blocks } = this.props;
+    const mappedBlocks = blocks.map(block => <li>block</li>)
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Infinitive scroll - Bojan Golubovic</h1>
         </header>
+        <ul>
+          {mappedBlocks}
+        </ul>
         <p className="App-intro">
         </p>
       </div>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,5 @@
 const initialState = {
-    blocks: 20
+    blocks: [1,2,3,4,5,6,7,8,9,10,11,12,13]
 }
 
 const reducer = (state = initialState, action) => {
